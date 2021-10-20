b6b85b30aa7a8a8a640cccf4324371006a69aab0
Violet Stewart <vkazoo2@gmail.com>
2021-10-20 15:02:51 -0700

    generate pokemon works

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 // import functions and grab DOM elements
+import { pokemon } from './pokemon.js';
 
-// initialize global state
+const poke1Img = document.getElementById('poke-1-img');
+const poke2Img = document.getElementById('poke-2-img');
+const poke3Img = document.getElementById('poke-3-img');
+const button = document.getElementById('select');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const generatePokemon = () => {
+
+    let randNum1 = Math.floor(Math.random() * pokemon.length);
+    let randNum2 = Math.floor(Math.random() * pokemon.length);
+    let randNum3 = Math.floor(Math.random() * pokemon.length);
+
+    while (
+        randNum1 === randNum2 ||
+  randNum1 === randNum3 ||
+  randNum2 === randNum3
+
+    ) {
+        randNum1 = Math.floor(Math.random () * pokemon.length);
+        randNum2 = Math.floor(Math.random() * pokemon.length);
+        randNum3 = Math.floor(Math.random() * pokemon.length);
+
+    }
+    //console.log(randNum1, randNum2, randNum3);
+    let poke1 = pokemon[randNum1];
+    poke1Img.src = poke1.url_image;
+
+    let poke2 = pokemon[randNum2];
+    poke2Img.src = poke2.url_image;
+
+    let poke3 = pokemon[randNum3];
+    poke3Img.src = poke3.url_image;
+};
+
+generatePokemon();
+
+button.addEventListener('click', ()=>{
+    generatePokemon();
+});
